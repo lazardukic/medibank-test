9855b38a00a47c589ae9162b3abd29ab57b01bec
Lazar <lazar.dukicc@gmail.com>
2023-12-04 15:05:06 +0100

    adding expected messages to tests

--- src/test/java/com/audition/integration/AuditionControllerTests.java
+++ src/test/java/com/audition/integration/AuditionControllerTests.java
@@ -9,7 +9,6 @@ import com.audition.service.AuditionService;
 import com.audition.web.AuditionController;
 import java.util.Collections;
 import java.util.List;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,80 +25,68 @@ class AuditionControllerTests {
     @MockBean
     private transient AuditionService auditionService;
 
+    private static final String EXPECTED_OK = "Expected OK response";
+    private static final String EXPECTED_BAD_REQUEST = "Expected BAD_REQUEST response";
+    private static final String EXPECTED_LIST = "Expected list from mocked service";
+    private static final String EXPECTED_AUDITION_POST = "Expected AuditionPost from mocked service";
+
     @Test
     void postsTest() {
         when(auditionService.getPosts()).thenReturn(Collections.emptyList());
-
         final ResponseEntity<List<AuditionPost>> responseEntity = auditionController.getPosts(null);
-
-        assertEquals(StringUtils.EMPTY, HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(StringUtils.EMPTY, Collections.emptyList(),
-            responseEntity.getBody());
+        assertEquals(EXPECTED_OK, HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(EXPECTED_LIST, Collections.emptyList(), responseEntity.getBody());
     }
 
     @Test
     void postsInvalidIdTest() {
         when(auditionService.getPosts()).thenReturn(Collections.emptyList());
-
         final ResponseEntity<List<AuditionPost>> responseEntity = auditionController.getPosts(-97);
-
-        assertEquals(StringUtils.EMPTY, HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals(EXPECTED_BAD_REQUEST, HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
     }
 
     @Test
     void postTest() {
         when(auditionService.getPostById(1)).thenReturn(new AuditionPost());
-
         final ResponseEntity<AuditionPost> responseEntity = auditionController.getPost(1);
-
-        assertEquals(StringUtils.EMPTY, HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(StringUtils.EMPTY, new AuditionPost(), responseEntity.getBody());
+        assertEquals(EXPECTED_OK, HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(EXPECTED_AUDITION_POST, new AuditionPost(), responseEntity.getBody());
     }
 
     @Test
     void postInvalidIdTest() {
         when(auditionService.getPostById(1)).thenReturn(new AuditionPost());
-
         final ResponseEntity<AuditionPost> responseEntity = auditionController.getPost(null);
-
-        assertEquals(StringUtils.EMPTY, HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals(EXPECTED_BAD_REQUEST, HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
     }
 
     @Test
     void postWithCommentsTest() {
         when(auditionService.getPostWithComments(1)).thenReturn(new AuditionPost());
-
         final ResponseEntity<AuditionPost> responseEntity = auditionController.getPostWithComments(1);
-
-        assertEquals(StringUtils.EMPTY, HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(StringUtils.EMPTY, new AuditionPost(), responseEntity.getBody());
+        assertEquals(EXPECTED_OK, HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(EXPECTED_AUDITION_POST, new AuditionPost(), responseEntity.getBody());
     }
 
     @Test
     void postWithCommentsInvalidIdTest() {
         when(auditionService.getPostWithComments(1)).thenReturn(new AuditionPost());
-
         final ResponseEntity<AuditionPost> responseEntity = auditionController.getPostWithComments(-97);
-
-        assertEquals(StringUtils.EMPTY, HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals(EXPECTED_BAD_REQUEST, HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
     }
 
     @Test
     void commentsByPostIdTest() {
         when(auditionService.getCommentsByPostId(1)).thenReturn(Collections.emptyList());
-
         final ResponseEntity<List<Comment>> responseEntity = auditionController.getCommentsByPostId(1);
-
-        assertEquals(StringUtils.EMPTY, HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(StringUtils.EMPTY, Collections.emptyList(), responseEntity.getBody());
+        assertEquals(EXPECTED_OK, HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals(EXPECTED_LIST, Collections.emptyList(), responseEntity.getBody());
     }
 
     @Test
     void commentsByPostIdInvalidIdTest() {
         when(auditionService.getCommentsByPostId(1)).thenReturn(Collections.emptyList());
-
         final ResponseEntity<List<Comment>> responseEntity = auditionController.getCommentsByPostId(null);
-
-        assertEquals(StringUtils.EMPTY, HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals(EXPECTED_BAD_REQUEST, HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
     }
 }
